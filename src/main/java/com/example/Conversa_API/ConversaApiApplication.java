2304bdf569eced97ce01f0017b6953df5bc1713c
Sahil Patel <sahilpatel1906@gmail.com>
2024-03-01 11:04:35 -0600

    First commit

---
+++ src/main/java/com/example/Conversa_API/ConversaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Conversa_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Conversa_API/ConversaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Conversa_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
