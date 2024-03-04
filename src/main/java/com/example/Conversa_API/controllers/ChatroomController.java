59c6ee1cc33bad56f91ce431f8ac8d0330b526c6
Jean <jean-ata@hotmail.co.uk>
2024-03-04 16:27:02 +0000

    add addNewChatroom @PostMapping to ChatroomController and Service

--- src/main/java/com/example/Conversa_API/controllers/ChatroomController.java
+++ src/main/java/com/example/Conversa_API/controllers/ChatroomController.java
@@ -1,6 +1,7 @@
 package com.example.Conversa_API.controllers;
 
 import com.example.Conversa_API.models.Chatroom;
+import com.example.Conversa_API.models.ChatroomDTO;
 import com.example.Conversa_API.services.ChatroomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,6 +31,12 @@ public class ChatroomController {
            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
        }
        return new ResponseEntity<>(chatroomService.findChatroomById(id), HttpStatus.OK);
+   }
+
+   @PostMapping
+    public ResponseEntity<Chatroom> addNewChatroom(@RequestBody ChatroomDTO chatroomDTO){
+        Chatroom chatroom = chatroomService.saveChatroom(chatroomDTO);
+        return new ResponseEntity<>(chatroom, HttpStatus.CREATED);
    }
 
 }
--- src/main/java/com/example/Conversa_API/services/ChatroomService.java
+++ src/main/java/com/example/Conversa_API/services/ChatroomService.java
@@ -1,6 +1,7 @@
 package com.example.Conversa_API.services;
 
 import com.example.Conversa_API.models.Chatroom;
+import com.example.Conversa_API.models.ChatroomDTO;
 import com.example.Conversa_API.repositories.ChatroomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,4 +22,11 @@ public class ChatroomService {
     public Optional<Chatroom> findChatroomById(Long id) {
         return chatroomRepository.findById(id);
     }
+
+    public Chatroom saveChatroom(ChatroomDTO chatroomDTO){
+        Chatroom chatroom = new Chatroom(chatroomDTO.getName());
+        chatroomRepository.save(chatroom);
+        return chatroom;
+    }
+
 }
