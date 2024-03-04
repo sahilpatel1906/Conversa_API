7de4f5cf13b205d8f2c2771c0f317d106851de6f
Jean <jean-ata@hotmail.co.uk>
2024-03-04 15:39:36 +0000

    add getAllChatrooms @GetMapping to ChatroomController and Service

--- src/main/java/com/example/Conversa_API/controllers/ChatroomController.java
+++ src/main/java/com/example/Conversa_API/controllers/ChatroomController.java
@@ -1,4 +1,28 @@
 package com.example.Conversa_API.controllers;
 
+import com.example.Conversa_API.models.Chatroom;
+import com.example.Conversa_API.services.ChatroomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("chatrooms")
 public class ChatroomController {
+
+    @Autowired
+    ChatroomService chatroomService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Chatroom>> getAllChatrooms(){
+       return new ResponseEntity<>(chatroomService.findAllChatrooms(), HttpStatus.OK);
+   }
+
 }
--- src/main/java/com/example/Conversa_API/services/ChatroomService.java
+++ src/main/java/com/example/Conversa_API/services/ChatroomService.java
@@ -1,4 +1,19 @@
 package com.example.Conversa_API.services;
 
+import com.example.Conversa_API.models.Chatroom;
+import com.example.Conversa_API.repositories.ChatroomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ChatroomService {
+
+    @Autowired
+    ChatroomRepository chatroomRepository;
+
+    public List<Chatroom> findAllChatrooms(){
+        return chatroomRepository.findAll();
+    }
 }
