400a0c920b89ce33e388d5ee5f4b4bc2302dabd9
Aebel Shajan <aebelshajan@gmail.com>
2024-03-04 11:46:28 +0000

    Finish repository setup

--- src/main/java/com/example/Conversa_API/repositories/ChatroomRepository.java
+++ src/main/java/com/example/Conversa_API/repositories/ChatroomRepository.java
@@ -1,4 +1,10 @@
 package com.example.Conversa_API.repositories;
 
-public interface ChatroomRepository {
+import com.example.Conversa_API.models.Chatroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
+
 }
--- src/main/java/com/example/Conversa_API/repositories/UserRepository.java
+++ src/main/java/com/example/Conversa_API/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.Conversa_API.repositories;
 
-public interface UserRepository {
+import com.example.Conversa_API.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
