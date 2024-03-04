241dae2abc1aa675824895a24392dd94c6ce65a0
Aebel Shajan <aebelshajan@gmail.com>
2024-03-04 17:58:28 +0000

    Add addNewMessage @PostMapping to MessageController

--- src/main/java/com/example/Conversa_API/controllers/MessageController.java
+++ src/main/java/com/example/Conversa_API/controllers/MessageController.java
@@ -1,15 +1,15 @@
 package com.example.Conversa_API.controllers;
 
 import com.example.Conversa_API.models.Message;
+import com.example.Conversa_API.models.MessageDTO;
+import com.example.Conversa_API.models.User;
 import com.example.Conversa_API.repositories.MessageRepository;
 import com.example.Conversa_API.services.MessageService;
+import com.example.Conversa_API.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,6 +34,12 @@ public class MessageController {
             return new ResponseEntity<>(foundMessage.get(), HttpStatus.OK);
         }
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<Message> addNewMessage(@RequestBody MessageDTO messageDTO) {
+        Message message = messageService.saveMessage(messageDTO);
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/example/Conversa_API/services/MessageService.java
+++ src/main/java/com/example/Conversa_API/services/MessageService.java
@@ -1,7 +1,12 @@
 package com.example.Conversa_API.services;
 
+import com.example.Conversa_API.models.Chatroom;
 import com.example.Conversa_API.models.Message;
+import com.example.Conversa_API.models.MessageDTO;
+import com.example.Conversa_API.models.User;
+import com.example.Conversa_API.repositories.ChatroomRepository;
 import com.example.Conversa_API.repositories.MessageRepository;
+import com.example.Conversa_API.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +19,33 @@ public class MessageService {
     @Autowired
     MessageRepository messageRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ChatroomRepository chatroomRepository;
+
     public List<Message> getAllMessages() {
         return messageRepository.findAll();
     }
 
     public Optional<Message> findMessage(long id) {
         return messageRepository.findById(id);
+    }
+
+    public Message saveMessage(MessageDTO messageDTO) {
+        Optional<User> userOptional = userRepository.findById(messageDTO.getUserId());
+        Optional<Chatroom> chatroomOptional = chatroomRepository.findById(messageDTO.getChatroomId());
+        User user = new User();
+        Chatroom chatroom = new Chatroom();
+        if (userOptional.isPresent() ) {
+            user = userOptional.get();
+        }
+        if (chatroomOptional.isPresent()) {
+            chatroom = chatroomOptional.get();
+        }
+        Message message = new Message(messageDTO.getMessage(), user, chatroom);
+        messageRepository.save(message);
+        return message;
     }
 }
