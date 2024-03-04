7818515ac1e1332f58d7287bccc2c4297eaea783
Sahil Patel <sahilpatel1906@gmail.com>
2024-03-04 11:21:29 -0600

    add getAllMessage route

--- src/main/java/com/example/Conversa_API/controllers/MessageController.java
+++ src/main/java/com/example/Conversa_API/controllers/MessageController.java
@@ -1,4 +1,28 @@
 package com.example.Conversa_API.controllers;
 
+import com.example.Conversa_API.models.Message;
+import com.example.Conversa_API.repositories.MessageRepository;
+import com.example.Conversa_API.services.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/messages")
 public class MessageController {
+
+    @Autowired
+    MessageService messageService;
+
+    @GetMapping(value = "/admin")
+    public ResponseEntity<List<Message>> getAllMessages(){
+        List<Message> messages = messageService.getAllMessages();
+        return new ResponseEntity<>(messages, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/example/Conversa_API/services/MessageService.java
+++ src/main/java/com/example/Conversa_API/services/MessageService.java
@@ -1,4 +1,19 @@
 package com.example.Conversa_API.services;
 
+import com.example.Conversa_API.models.Message;
+import com.example.Conversa_API.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MessageService {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
 }
