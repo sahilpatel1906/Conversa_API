99d2df23914a9e4c69a538ea04fc20254ed4eae9
Jean <jean-ata@hotmail.co.uk>
2024-03-04 13:57:39 +0000

    add properties to the ChatroomDTO

--- src/main/java/com/example/Conversa_API/models/ChatroomDTO.java
+++ src/main/java/com/example/Conversa_API/models/ChatroomDTO.java
@@ -1,4 +1,35 @@
 package com.example.Conversa_API.models;
 
+import java.util.List;
+
 public class ChatroomDTO {
+
+    private String name;
+
+    private List<Message> messageIds;
+
+    public ChatroomDTO() {
+    }
+
+    public ChatroomDTO(String name, List<Message> messageIds) {
+        this.name = name;
+        this.messageIds = messageIds;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Message> getMessageIds() {
+        return messageIds;
+    }
+
+    public void setMessageIds(List<Message> messageIds) {
+        this.messageIds = messageIds;
+    }
 }
