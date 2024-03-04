94aedcf5741428a712ad2b7fc8a999f32b8169f8
Aebel Shajan <aebelshajan@gmail.com>
2024-03-04 13:27:50 +0000

    Add methods and properties to chatroom model

--- src/main/java/com/example/Conversa_API/models/Chatroom.java
+++ src/main/java/com/example/Conversa_API/models/Chatroom.java
@@ -1,4 +1,56 @@
 package com.example.Conversa_API.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "chatrooms")
 public class Chatroom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    @OneToMany(mappedBy = "chatroom")
+    @JsonIgnoreProperties({"chatroom"})
+    private List<Message> messages;
+
+    public Chatroom() {
+    }
+
+    public Chatroom(String name, List<Message> messages) {
+        this.name = name;
+        this.messages = messages;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
 }
